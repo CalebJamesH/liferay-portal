6ce6c5b133007b542f1c3aa38fe950d705a80ef5
Caleb Hall <caleb.hall@liferay.com>
2026-03-02 15:41:51 -0300

    LPD-77976 Create object action file for analytics provisioning

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-etc-spring-boot/src/main/java/com/liferay/marketplace/AnalyticsRestController.java
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-etc-spring-boot/src/main/java/com/liferay/marketplace/AnalyticsRestController.java
@@ -6,7 +6,6 @@
 package com.liferay.marketplace;
 
 import com.liferay.client.extension.util.spring.boot3.BaseRestController;
-import com.liferay.headless.commerce.admin.order.client.dto.v1_0.Order;
 import com.liferay.marketplace.constants.MarketplaceConstants;
 import com.liferay.marketplace.service.KoroneikiService;
 import com.liferay.marketplace.service.MarketplaceService;
@@ -18,13 +17,11 @@ import com.liferay.osb.koroneiki.phloem.rest.client.pagination.Pagination;
 import com.liferay.osb.koroneiki.phloem.rest.client.resource.v1_0.ProductPurchaseResource;
 import com.liferay.osb.koroneiki.phloem.rest.client.resource.v1_0.ProductResource;
 import com.liferay.petra.string.StringBundler;
-import com.liferay.portal.kernel.util.HashMapBuilder;
 
 import java.time.Duration;
 
 import java.util.Base64;
 import java.util.Date;
-import java.util.Map;
 import java.util.Objects;
 
 import org.apache.commons.logging.Log;
@@ -34,19 +31,13 @@ import org.json.JSONObject;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
-import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import reactor.util.retry.Retry;
@@ -177,90 +168,6 @@ public class AnalyticsRestController extends BaseRestController {
 				"/o/faro/main/project/" + projectId
 			).build(
 			).toUri());
-	}
-
-	@PostMapping("provisioning")
-	public void postProvisioning(@RequestBody String json) throws Exception {
-		JSONObject commerceOrderJSONObject = new JSONObject(
-			json
-		).getJSONObject(
-			"commerceOrder"
-		);
-
-		Order order = _marketplaceService.getOrder(
-			commerceOrderJSONObject.getLong("id"));
-
-		if (_log.isInfoEnabled()) {
-			_log.info("Provisioning order " + order.getId());
-		}
-
-		Map<String, String> customFields =
-			(Map<String, String>)order.getCustomFields();
-
-		JSONObject orderMetadataJSONObject = new JSONObject(
-			customFields.getOrDefault("order-metadata", "{}"));
-
-		JSONObject analyticsFormJSONObject =
-			orderMetadataJSONObject.optJSONObject("analyticsForm");
-
-		String response = WebClient.builder(
-		).baseUrl(
-			_analyticsAuthUrl
-		).defaultHeader(
-			HttpHeaders.AUTHORIZATION, _getAuthorization()
-		).build(
-		).post(
-		).uri(
-			"/o/faro/main/project/unprovisioned"
-		).contentType(
-			MediaType.APPLICATION_FORM_URLENCODED
-		).body(
-			BodyInserters.fromFormData(
-				"corpProjectName",
-				analyticsFormJSONObject.getString("corpProjectName")
-			).with(
-				"corpProjectUuid",
-				analyticsFormJSONObject.getString("corpProjectUuid")
-			).with(
-				"incidentReportEmailAddresses",
-				analyticsFormJSONObject.getJSONArray(
-					"incidentReportEmailAddresses"
-				).toString()
-			).with(
-				"name", analyticsFormJSONObject.getString("name")
-			).with(
-				"serverLocation",
-				analyticsFormJSONObject.optString(
-					"serverLocation", "us-west1-ac-uat-c1")
-			).with(
-				"sharedCluster", "false"
-			).with(
-				"trial", "true"
-			).with(
-				"ownerEmailAddress",
-				analyticsFormJSONObject.getString("ownerEmailAddress")
-			)
-		).retrieve(
-		).bodyToMono(
-			String.class
-		).block();
-
-		if (response == null) {
-			return;
-		}
-
-		if (_log.isInfoEnabled()) {
-			_log.info("Analytics project created for order " + order.getId());
-		}
-
-		_marketplaceService.updateOrder(
-			HashMapBuilder.put(
-				"order-metadata",
-				orderMetadataJSONObject.put(
-					"analyticsProject", new JSONObject(response)
-				).toString()
-			).build(),
-			order.getId(), MarketplaceConstants.ORDER_STATUS_COMPLETED);
 	}
 
 	@Override
