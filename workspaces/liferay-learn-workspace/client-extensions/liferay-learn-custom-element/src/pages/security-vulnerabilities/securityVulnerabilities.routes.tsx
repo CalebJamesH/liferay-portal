faad162195a62b5746f2fb1cfe16d71f4db751bd
Caleb Hall <caleb.hall@liferay.com>
2026-07-30 11:19:19 -0300

    DG-2598 migrate security-vulnerabilities

---
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-custom-element/src/pages/security-vulnerabilities/securityVulnerabilities.routes.tsx
@@ -0,0 +1,24 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {HashRouter, Route, Routes} from 'react-router-dom';
+
+import SecurityVulnerabilitiesList from './SecurityVulnerabilitiesList';
+import SecurityVulnerabilitiesItem from './SecurityVulnerabilitiesItem';
+
+import './SecurityVulnerabilities.css';
+
+const SecurityVulnerabilitiesRoutes = () => {
+	return (
+		<HashRouter>
+			<Routes>
+				<Route element={<SecurityVulnerabilitiesList />} path="/" />
+				<Route element={<SecurityVulnerabilitiesItem />} path="/:id" />
+			</Routes>
+		</HashRouter>
+	);
+};
+
+export default SecurityVulnerabilitiesRoutes;
